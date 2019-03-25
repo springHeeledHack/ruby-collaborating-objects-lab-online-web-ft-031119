02440f1d12aed3fa6c2759cde9c128cb5372a6f5
kyle smith <kylemclellan@gmail.com>
2019-03-25 21:50:13 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,9 +20,9 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    @@all.each do |artist| 
-      if artist.name == name 
-        return artist
+    @@all.each do |entry| 
+      if entry.name == name 
+        return entry
       end
     end
     Artist.new(name)
